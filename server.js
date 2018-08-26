4f84f971e5fb74d10e4dc2e060837498703b2161
Divyanshu Rawat <divyanshu.r46956@gmail.com>
2018-08-26 18:32:41 +0530

    added docker file

--- server.js
+++ server.js
@@ -11,3 +11,9 @@ app.get('/', function (req, res) {
 app.listen(8081, function () {
   console.log('app listening on port 8081!')
 })
+
+
+// Every application requires a specific working environment: pre-installed applications, dependencies, data bases, everything in specific version.
+// Docker containers allow you to create such environments.
+// Contrary to VM, however, the container doesn’t hold the whole operating system—just applications, 
+// just applications, dependencies, and configuration. This makes Docker containers much lighter and faster than regular VM’s.
