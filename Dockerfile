ea5c93070b1eb807e2b1d3d11a0164f7025cb4a7
Divyanshu Rawat <divyanshu.r46956@gmail.com>
2018-08-26 22:35:31 +0530

    port modified

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,5 @@ RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
 COPY . .
 RUN npm install
-EXPOSE 8081
+EXPOSE 3000
 CMD [ "node", "server.js" ]
--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.get('/', function (req, res) {
   res.send('Getting Started With Docker !!:)')
 })
 
-app.listen(8081, function () {
-  console.log('app listening on port 8081!')
+app.listen(3000, function () {
+  console.log('app listening on port 3000!')
 })
 
 
